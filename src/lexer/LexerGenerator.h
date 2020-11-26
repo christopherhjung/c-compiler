07b2dcdc4b1553c2fd72478ae0d90cb9ec81a7a6
Christopher Jung <christopher.h.jung@web.de>
2020-11-26 13:25:51 +0100

    remove unused code

--- src/lexer/LexerGenerator.h
+++ src/lexer/LexerGenerator.h
@@ -94,8 +94,8 @@ public:
 
 
         offset(2) << "while(true){" << std::endl;
-        offset(3) << "token.location.line = line;" << std::endl;
-        offset(3) << "token.location.column = column;" << std::endl;
+        //offset(3) << "token.location.line = line;" << std::endl;
+        //offset(3) << "token.location.column = column;" << std::endl;
         offset(3) << "if(current == 256){" << std::endl;
         offset(4) << "if(finish){" << std::endl;
         offset(5) << "return false;" << std::endl;
